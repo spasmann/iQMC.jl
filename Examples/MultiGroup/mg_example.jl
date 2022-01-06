c42080d33641b1cbe431211fd61f57079477db56
Tim Kelley <ctk@ncsu.edu>
2022-01-06 15:45:59 -0500

    update

--- Examples/MultiGroup/mg_example.jl
+++ Examples/MultiGroup/mg_example.jl
@@ -8,13 +8,16 @@ Nx=40; LB=0.0; RB=5.0;
 geometry = "Slab"
 generator = "Sobol"
 G=12
-N=2^8
+N=2^11
 qmc_data = multiGroup_init(geometry, generator, N, LB, RB, Nx, G)
 exact=qmc_data.true_flux
 mxv_data = SamInitMV(qmc_data)
 gmsol=Solver_Compare(mxv_data; plabel="$G group problem")
-println(size(exact))
-println(size(gmsol))
-println(norm(exact-gmsol,Inf))
+solex=ones(Nx,G)
+for ig=1:G
+   solex[:,ig].=exact[ig]
+end
+vsolex=reshape(solex,G*Nx,)
+println(norm(vsolex-gmsol,Inf))
 end
 
