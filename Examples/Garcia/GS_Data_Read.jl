5a435be15aff2ad59d2c345033bf15f03b5734ec
spasmann <46267220+spasmann@users.noreply.github.com>
2022-06-02 13:16:11 -0600

    Generalized the filename in GS_Data_Read

--- Examples/Garcia/GS_Data_Read.jl
+++ Examples/Garcia/GS_Data_Read.jl
@@ -1,7 +1,10 @@
 function GS_Data_Read(s)
 DataGS=zeros(11,2);
 s == 1.0 ? sstr=1 : sstr=Inf
-fname="/Users/sampasmann/Documents/GitHub/Krylov_QMC/Examples/Garcia_Scripts/Siewert:s=$sstr.dat"
+dir = @__DIR__
+file = "/Examples/Garcia_Scripts/Siewert:s=$sstr.dat"
+#fname="/Users/sampasmann/Documents/GitHub/Krylov_QMC/Examples/Garcia_Scripts/Siewert:s=$sstr.dat"
+fname = dir*file
 readtab(fname,DataGS)
 return DataGS
 end
--- Examples/Garcia/garcia_example.jl
+++ Examples/Garcia/garcia_example.jl
@@ -1,10 +1,10 @@
 function garcia_example()
 generator = "Sobol"
 geometry = "Slab"
-Nx=160
-N=2^11
-na2 = 11    # number of angles for angular mesh
-s = 1.0     # parameter in Garcia/Siewert
+Nx=50      # number of spatial cells
+N=2^11     # number of particles per QMC sweep
+na2 = 11   # number of angles for angular mesh
+s = 1.0    # parameter in Garcia/Siewert
 LB = 0.0   # left bound
 RB = 5.0   # right bound
 qmc_data = garcia_init(geometry, generator, N, LB, RB, Nx, na2, s)
