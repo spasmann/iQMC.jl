a1431ec6d54cab5c96b0976027ea3e4d3c5ff07d
spasmann <46267220+spasmann@users.noreply.github.com>
2022-06-03 15:36:46 -0600

    Director fix for GS_Data_Read

--- Examples/Garcia/GS_Data_Read.jl
+++ Examples/Garcia/GS_Data_Read.jl
@@ -2,7 +2,7 @@ function GS_Data_Read(s)
 DataGS=zeros(11,2);
 s == 1.0 ? sstr=1 : sstr=Inf
 dir = @__DIR__
-file = "/../Garcia_Scripts/Siewert:s=$sstr.dat"
+file = "/Siewert:s=$sstr.dat"
 #fname="/Users/sampasmann/Documents/GitHub/Krylov_QMC/Examples/Garcia_Scripts/Siewert:s=$sstr.dat"
 fname = dir*file
 readtab(fname,DataGS)
