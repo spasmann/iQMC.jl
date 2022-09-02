cc3f8c5c1ceb65b8199ed00a35f138097ac47a4f
spasmann <46267220+spasmann@users.noreply.github.com>
2022-09-02 16:53:18 -0600

    Delete Examples directory

--- Examples/Reeds/compare_residual.jl
+++
@@ -1,6 +0,0 @@
-push!(LOAD_PATH,"/Users/sampasmann/Documents/GitHub/Krylov_QMC/Examples/")
-using Krylov_Test
-
-data = reeds_init("Sobol", 2^12, 180)
-mxv_data = SamInitMV(data)
-gmres_sol = Krylov_Test.Solver_Compare(mxv_data, plabel="Reed's Problem")
